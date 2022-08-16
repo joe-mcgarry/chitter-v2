20010a47d9732c4eaa80287ef72832e5a6d193c6
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-16 16:32:57 +0100

    adds the user and peep models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "peeps#index"
 
   get "/peeps", to: "peeps#index"
 end
---
+++ db/migrate/20220816153223_create_peeps.rb
@@ -0,0 +1,11 @@
+class CreatePeeps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :peeps do |t|
+      t.string :user
+      t.date :date
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
