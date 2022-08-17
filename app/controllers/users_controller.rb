6b6726edc89d7486ead6174cb1e7375697d93b82
Joe McGarry <joewmcgarry@gmail.com>
2022-08-17 17:12:49 +0100

    Adds create route to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    
   end
 
   def create
-    
+    @user = User.new(params[:first_name][:last_name][:handle][:email][:password])
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
