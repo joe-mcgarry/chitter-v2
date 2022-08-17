14907e9564dc1a8b4c9c9449cc8b43f87d25fe3c
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-17 14:11:56 +0100

    here you go

--- test/models/peep_test.rb
+++ test/models/peep_test.rb
@@ -1,10 +1,17 @@
 require "test_helper"
 
 class PeepTest < ActiveSupport::TestCase
-  test "should not save article without body" do
+  test "should not save peep without body" do
     peep = Peep.new
     assert_not peep.save, "You failed"
   end
-end
+
+  test "peep body is saved" do
+    peep = Peep.new(user: "Joe", date: Date.new(2022,8,17), body: "Sophie is the best")
+    peep.save
+    assert_equal peep.all[0].body
+
+
+  end
 
 
