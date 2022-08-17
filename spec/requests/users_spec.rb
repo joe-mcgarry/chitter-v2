db55d143310543b3bf3d7fe5686c3da394cae370
Joe McGarry <joewmcgarry@gmail.com>
2022-08-17 17:06:53 +0100

    Changes form to Rails form builder

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
+  describe "GET /users/new" do
     it "returns http success" do
-      get "/users"
+      get "/users/new"
       expect(response).to have_http_status(:success)
     end
   end
