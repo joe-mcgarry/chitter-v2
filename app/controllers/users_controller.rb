70a8c8a979d4676b51dd0fbad764a918cc0b980f
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-17 17:18:05 +0100

    adds a user to the new route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
   end
 
   def new
-    
+    @user = User.new
   end
 
   def create
+    #update this section to specify which param goes to which field
     @user = User.new(params[:first_name][:last_name][:handle][:email][:password])
     if @user.save
       redirect_to @user
