80584725d3de1620eca0945ba7277c2f382efdc1
Joe McGarry <joewmcgarry@gmail.com>
2022-09-12 10:51:35 +0100

    Adds method and view to create post, adds validation for minimum length to be 10

--- app/controllers/peeps_controller.rb
+++ app/controllers/peeps_controller.rb
@@ -6,4 +6,23 @@ class PeepsController < ApplicationController
   def show
     @peep = Peep.find(params[:id])
   end
+
+  def new
+    @peep = Peep.new
+  end
+
+  def create
+    @peep = Peep.new(peep_params)
+
+    if @peep.save
+      redirect_to @peep
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def peep_params
+    params.require(:peep).permit(:body)
+  end
 end
